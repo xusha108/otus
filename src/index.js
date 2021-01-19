a855d5387ff9c77f9d4fc1de6d915ef8f2e86873
unknown <xusha108@gmail.com>
2021-01-19 10:41:00 +0300

    webpack babel

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./components/App.js";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const App = () => {
+  return(
+    <div>
+      <p>Hello Changes</p>      
+    </div>
+  )
+}
+
+export default App;
+
+ReactDOM.render(<App />, document.getElementById("root"))
--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,41 @@
+const HtmlPlugin = require("html-webpack-plugin");
 const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: "./src/index.js",
   output: {
-    path: path.join(__dirname, "/dist"),
-    filename: "index-bundle.js"
-  },
-  module: {
+    path: path.resolve(__dirname, "build"),
+    filename: "bundle.js"
+  }, 
+  module: {    
     rules: [
       {
-        test: /\.js$/,
+        test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        use: ["babel-loader"]
+        use: [
+          {
+            loader:"babel-loader"
+          }
+        ]
       },
       {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"]
-      }
+        test: /\.html$/,      
+        use: [
+          {
+            loader:"html-loader"
+          }
+        ]
+      },
     ]
   },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 9000
+  },
   plugins: [
-    new HtmlWebpackPlugin({
-      template: "./src/index.html"
+    new HtmlPlugin({
+      filename: "index.html",
+      template: "index.html"
     })
   ]
-};
+}
