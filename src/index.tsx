2b8e55946a3dbd34c2a4caa3b8bf44fcfb5f915a
unknown <xusha108@gmail.com>
2021-01-20 13:55:27 +0300

    add priettier jest

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,12 @@
-import React, {FC} from 'react';
-import ReactDOM from 'react-dom';
+import React, { FC } from "react";
+import ReactDOM from "react-dom";
 
-const App: FC = () => {    
-  return(
-   <div>
-      <h1>Hello World</h1>     
-  </div>  
-  )
-}
+const App: FC = () => {
+  return (
+    <div>
+      <h1>Hello World</h1>
+    </div>
+  );
+};
 
-export default App;
-
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(<App />, document.getElementById("root"));
--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,41 @@
-const HtmlPlugin = require('html-webpack-plugin');
-const path = require('path');
+const HtmlPlugin = require("html-webpack-plugin");
+const path = require("path");
 
 module.exports = {
-  entry: './src/index.tsx',
+  entry: "./src/index.tsx",
   resolve: {
-    extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
+    extensions: [".js", ".jsx", ".ts", ".tsx", ".json"],
   },
   output: {
-    path: path.resolve(__dirname, 'build'),
-    filename: 'bundle.js'
-  }, 
-  module: {    
+    path: path.resolve(__dirname, "build"),
+    filename: "bundle.js",
+  },
+  module: {
     rules: [
       {
         test: /\.(js|jsx|ts|tsx)$/,
         exclude: /node_modules/,
-        use: [{
-          loader:'babel-loader' ,
-          loader: 'eslint-loader',
-          loader: 'ts-loader'
-        }]
-      },   
-      {
-        test: /\.html$/,      
-        use: 'html-loader'
-          
+        use: [
+          {
+            loader: "babel-loader",
+            loader: "eslint-loader",
+            loader: "ts-loader",
+          },
+        ],
       },
-    ]
+      {
+        test: /\.html$/,
+        use: "html-loader",
+      },
+    ],
   },
   devServer: {
-    port: 8080
+    port: 8080,
   },
   plugins: [
     new HtmlPlugin({
-      filename: 'index.html',
-      template: 'index.html'
-    })
-  ]
-}
+      filename: "index.html",
+      template: "index.html",
+    }),
+  ],
+};
