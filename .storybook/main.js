7be2adec2f539e5be48caf0480febe587f55745b
unknown <xusha108@gmail.com>
2021-02-01 13:04:55 +0300

    Cannot read property tap of undefined

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const webpack = require('webpack');
+const custom = require('../webpack.config.js');
 
 module.exports = {
   stories: [ '../src/**/*.stories.@(js|jsx|ts|tsx)'],
@@ -7,5 +9,30 @@ module.exports = {
     '@storybook/addon-storysource',
     '@storybook/addon-actions',
     '@storybook/addon-knobs/register'
-  ]
+  ],
+  webpackFinal: (config) => {
+    config.plugins.push(new webpack.HotModuleReplacementPlugin());
+
+    config.module.rules.push({
+      test: /\.stories\.tsx$/,
+      loaders: [
+        {
+          loader: require.resolve('@storybook/source-loader'),
+          options: { parser: 'typescript' },
+        },
+      ],
+      enforce: 'pre',
+    });
+    
+    return {
+      ...config,
+      resolve: {
+        extensions: custom.resolve.extensions
+      },
+      module: {
+        ...config.module,
+        rules: [...config.module.rules, ...custom.module.rules],
+      },
+    };
+  }
 }
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,3 +1,2 @@
-import React from 'react';
-import './button.css';
-
+import React from "react";
+import "./button.css";
